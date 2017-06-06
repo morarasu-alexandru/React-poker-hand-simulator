aae4954a2a9e03cf5c7ea6155a5013981f32cf57
morarasu-alexandru <morarasu_alexandru@yahoo.com>
2017-06-06 22:17:21 +0300

    dev

---
+++ src/routes/Home/components/HandInsert.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import HandInsertInput from './HandInsertInput'
+
+const HandInsert = () => {
+  return (
+    <div className='HandInsert'>
+      <h2 className='Title'>Hand Insert</h2>
+      <HandInsertInput />
+    </div>
+  )
+}
+
+export default HandInsert
---
+++ src/routes/Home/components/HandInsertInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HandInsertInput = () => {
+  return(
+    <input type='text' value='ceva'/>
+  )
+}
+
+export default HandInsertInput
